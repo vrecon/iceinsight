9dfa3aa83b3e0c1ae60e6360bb8986048be77aa4
vrecon <git@vrecon.nl>
2026-08-25 18:19:54 +0000

    Fix Sporthive practice import (Origin header, yearly batches, max 500)

--- src/main/java/nl/templify/iceinsights/config/WebClientConfig.java
+++ src/main/java/nl/templify/iceinsights/config/WebClientConfig.java
@@ -2,21 +2,25 @@ package nl.templify.iceinsights.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    
+
+    private static final int MAX_IN_MEMORY_SIZE = 16 * 1024 * 1024;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-            .baseUrl("https://practice-api.speedhive.com")
-            .defaultHeaders(headers -> {
-                headers.set("Host", "practice-api.speedhive.com");
-                headers.set("Origin", "https://sporthive.com");
-                headers.set("Cookie", "ai_session=lx8/dg63CnVGw8aE6WGpaw|1729847176437|1729848795986; " +
-                          "ai_user=8XSUDiJIoix3QSuWx5gS3T|2024-10-25T09:06:15.656Z");
-            })
-            .build();
+                .baseUrl("https://practice-api.speedhive.com")
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .defaultHeaders(headers -> {
+                    headers.set("Origin", "https://sporthive.com");
+                    headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+                    headers.set(HttpHeaders.REFERER, "https://sporthive.com/");
+                })
+                .build();
     }
 }
